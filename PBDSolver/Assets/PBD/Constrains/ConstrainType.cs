a58bb04649fa5d2aa1cf9ad784cd465fcdfaac66
blueinsert <1401597377@qq.com>
2024-08-05 22:26:02 +0800

    new

---
+++ PBDSolver/Assets/PBD/Constrains/ConstrainType.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace bluebean.UGFramework.Physics
+{
+    public enum ConstrainType
+    {
+        None = -2,
+        
+        Collide,
+        Start,
+        Volume,
+        Stretch,
+        Max,
+    }
+}
